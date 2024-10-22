301cb1c6a2fdc30e6d3bd5550c28e4e8e9a5967e
josuegomez <josue.gomez@mavs.uta.edu>
2024-10-21 22:55:10 -0500

    feat: added functionality to create and view boards with unsplash api for photos

--- actions/create-board/schema.ts
+++ actions/create-board/schema.ts
@@ -4,7 +4,11 @@ export const CreateBoard = z.object({
   title: z
     .string({
       required_error: "Title is required",
-      invalid_type_error: "Title is required.",
+      invalid_type_error: "Invalid type.",
     })
     .min(3, { message: "Title must be at least 3 characters." }),
+  image: z.string({
+    required_error: "Image is required",
+    invalid_type_error: "Invalid type.",
+  }),
 });
