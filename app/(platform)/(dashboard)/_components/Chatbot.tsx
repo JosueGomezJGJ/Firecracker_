31b727c322663091feb667b370e05727de1ae666
Asmin Pothula <axp4488@mavs.uta.edu>
2024-11-10 23:42:13 +0000

    chatbot added (asmin honors)

---
+++ app/(platform)/(dashboard)/_components/Chatbot.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Chatbot = () => {
+    return (
+        <iframe
+            src="https://upgraded-winner-ggqr4v99gghvjw-5000.app.github.dev/" // URL of your Flask chatbot app
+            width="800px"
+            height="800px"
+            style={{ border: 'none' }}
+            title="Chatbot"
+        ></iframe>
+    );
+};
+
+export default Chatbot;
--- app/(platform)/(dashboard)/layout.tsx
+++ app/(platform)/(dashboard)/layout.tsx
@@ -1,10 +1,15 @@
 import { Navbar } from "./_components/navbar";
+import Chatbot from "./_components/Chatbot";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
       <Navbar />
       {children}
+      <Chatbot /> 
+      <div className="fixed bottom-4 right-4">
+        <Chatbot />
+      </div>
     </div>
   );
 };
